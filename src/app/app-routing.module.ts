e3c88a94da0b5d7f1114a4e5db7d6861c3f784b2
Shomou <edmundo.jayr.perez.rosales@gmail.com>
2025-09-09 22:14:37 -0600

    Se ponen los cambios del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MainLayoutComponent } from '../app/components/layout/main-layout/main-layout.component'
+const routes: Routes = [ 
+  // Ruta principal que usa el MainLayoutComponent
+  // Todas las rutas hijas se renderizarán dentro de este layout
+  {
+    path: '',
+    component: MainLayoutComponent,
+    // canActivate: [AuthGuard], // Futuro: aquí irá un guard para proteger las rutas
+    children: [
+      {
+        path:'',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./components/paginas/dashboard/dashboard.module').then(m => m.DashboardModule)
+      }, 
+      {
+        path: 'pacientes',
+        loadChildren: () => import('./components/paginas/pacientes/pacientes.module').then(m => m.PacientesModule)
+      },
+      {
+        path: 'dentistas',
+        loadChildren: () => import('./components/paginas/odontologos/dentistas.module').then(m => m.DentistasModule)
+      }]
+   } 
+  ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { MainLayoutComponent } from './components/layout/main-layout/main-layout.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
